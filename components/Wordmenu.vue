e5d063fd3bf94ad6b9c4b584c55438a0ff6e1c60
Emil Avara <emilavara@hotmail.com>
2024-01-27 14:49:49 +0100

    Added word rain to menu

---
+++ components/Wordmenu.vue
@@ -0,0 +1,54 @@
+<template>
+    <span :style="{left: word.ypos + '%'}" class="word-menu">
+        {{word.word}}
+    </span>
+</template>
+
+<script>
+import { gsap } from 'gsap'
+
+    export default {
+        props: [
+            'word'
+        ],
+        mounted() {
+
+        }
+    }
+</script>
+
+<style lang="scss">
+.word-menu {
+    color: white;
+    opacity: 0.25;
+    pointer-events: none;
+    position: absolute;
+    top: 0%;
+    width: fit-content;
+    height: 0;
+    animation: collision-menu 15s linear forwards;
+    // transition: top 30s;
+    /* top: 100%; */
+
+    @keyframes collision-menu {
+        0% {
+            top: -2%;
+        }
+        49% {
+            top: 49%;
+        }
+        50% {
+            top: 50%;
+        }
+        74% {
+            top: 74%;
+        }
+        75% {
+            top: 75%;
+        }
+        100% {
+            top: 100%;
+        }
+    }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -6,9 +6,41 @@
 			<span>how to play</span>
 			<span>options</span>
 		</div>
+    	<Wordmenu :word="word" v-for="word, index in spawned" :key="index"/>
+
 	</div>
 </template>
 
 <script>
+import wordlist from '~/components/wordlist.json';
 
+export default {
+	data() {
+		return {
+			words: wordlist,
+            spawned: [
+            ],
+            running: ''
+
+		}
+	},
+	methods: {
+		startGame() {
+            this.running = setInterval(this.spawnWord, 1000);
+        },
+		spawnWord() {
+            var r = Math.floor(Math.random()*this.words.length);
+            var rYPos = Math.floor(Math.random() * 90);
+
+            var spawnedObj = {}
+            spawnedObj.word = this.words[r]
+            spawnedObj.ypos = rYPos
+
+            this.spawned.push(spawnedObj)
+        },
+	},
+	mounted() {
+		this.startGame();
+	}
+}
 </script>
